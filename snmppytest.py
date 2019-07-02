b416b16b0ea6130bd6c04c05a77c8082b5d058d3
mzzhmh <mzzhmh@hotmail.com>
2019-07-02 23:42:21 +1000

    Update snmppytest.py

--- snmppytest.py
+++ snmppytest.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3.6
 from easysnmp import Session
 
-session = Session(hostname='10.13.17.57', community='BANMSRO', version=2)
+session = Session(hostname='10.13.17.57', community='public', version=2)
 SN = session.get('.1.3.6.1.2.1.47.1.1.1.1.11.1')
 SW = session.get('.1.3.6.1.2.1.47.1.1.1.1.9.1')
 print(SN.value)
 print(SW.value)
 
-session2 = Session(hostname='10.15.200.151', community='BANMSRO', version=2)
+session2 = Session(hostname='10.15.200.151', community='public', version=2)
 SN2 = session2.get('.1.3.6.1.2.1.47.1.1.1.1.11.1')
 SW2 = session2.get('.1.3.6.1.2.1.47.1.1.1.1.9.1')
 print(SN2.value)
 print(SW2.value)
 
-session3 = Session(hostname='10.15.200.152', community='BANMSRO', version=2)
+session3 = Session(hostname='10.15.200.152', community='public', version=2)
 SN3 = session3.get('.1.3.6.1.2.1.47.1.1.1.1.11.1')
 SW3 = session3.get('.1.3.6.1.2.1.47.1.1.1.1.9.1')
 print(SN3.value)
